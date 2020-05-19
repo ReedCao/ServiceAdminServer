acc32eb00b5e44bad7352424292f20c75deb16a4
Reed <caochongyu@gmail.com>
2020-05-19 16:05:00 +0800

    Initial commit

---
+++ service-admin/src/main/java/com/reed/serviceadmin/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.reed.serviceadmin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.UUID;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AdminServerProperties adminServer;
+
+    public WebSecurityConfig(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler =
+                new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.getContextPath() + "/");
+
+        http
+                .authorizeRequests()
+                .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
+                .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage(this.adminServer.getContextPath() + "/login")
+                .successHandler(successHandler)
+                .and()
+                .logout()
+                .logoutUrl(this.adminServer.getContextPath() + "/logout")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers("/applications","/applications/*","/instances","/instances/*","/actuator/**")
+                .and()
+                .rememberMe()
+                .key(UUID.randomUUID().toString())
+                .tokenValiditySeconds(1209600);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+}
